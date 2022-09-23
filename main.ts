9aed1b8f5beac607a63d82c1ad14ac746e47a629
majuelaura <114260493+majuelaura@users.noreply.github.com>
2022-09-23 17:38:45 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+let caxote = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("auau miau")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Silly)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    caxote += 1
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
